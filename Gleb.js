f11e926ddce6bb8d4f6b66decd01aacc2e221c05
--local <KopylovPL@mosreg.ru>
2020-03-19 11:59:46 +0300

    new script

--- Gleb.js
+++ Gleb.js
@@ -1,21 +1,13 @@
 var idArr = [
-"5e2ee10d22e5c60001ec20d0",
-"5e30167722e5c60001fbd92e",
-"5e30284b22e5c60001fdb4f4",
-"5e302acc22e5c60001fdf6bb",
-"5e30684022e5c6000103824b",
-"5e32e2cf22e5c6000124c4fa",
-"5e351a5f22e5c60001c0805d",
-"5e35636622e5c60001c54d6a",
-"5e3569e022e5c60001c5dad9",
-"5e35991b22e5c60001c90f48",
-"5e36b0d722e5c60001cf748c",
-"5e372d8e22e5c60001d19628",
-"5e37d15d22e5c60001d555b7",
-"5e37d76222e5c60001d5fc9b"
+    "5e2aec9622e5c60001d1735c",
+    "5e285e5f22e5c60001aa405c",
+    "5e286f3022e5c60001abf074",
+    "5e2c05e122e5c60001d925a6",
+    "1cee7284-3856-4e2b-a031-5d8bff918956",
+    "e597693e-9719-41a5-a8a7-8748d9f29650",    
 ];
 function getId(id) {
-    if (id.length = 24) {
+    if (id.length == 24) {
         return ObjectId(id);
     } else {
         return id;
