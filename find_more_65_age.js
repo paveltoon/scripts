7f0e0416496cbbf25b39fd091b6d59278219a4ba
--local <KopylovPL@mosreg.ru>
2020-05-27 19:17:51 +0300

    [fix] Адреса по красоте

--- find_more_65_age.js
+++ find_more_65_age.js
@@ -1,12 +1,12 @@
 function getAge(date) {
     return ((new Date('2020-06-24').getTime() - new Date(date)) / (24 * 3600 * 365.25 * 1000)) | 0;
 }
-
+print('Фамилия;Имя;Отчество;Паспорт серия;Пасопрт номер;СНИЛС;Адрес;Дата рождения;Телефон;Email')
 db.persons.find({
   "dateOfBirth": {
     $exists: true
   }
-}).addOption(DBQuery.Option.noTimeout).forEach(function (person) {
+}).addOption(DBQuery.Option.noTimeout).limit(160000).forEach(function (person) {
   var id = person._id;
   var age = getAge(person.dateOfBirth);
   if (age >= 65 && age < 160) {
@@ -62,23 +62,27 @@ db.persons.find({
             "corps": address.corps,
             "room": address.room
           }
-          if (address.regionPrefix != undefined) {
-            addressObj.region = `${address.regionPrefix}. ${address.region}`;
+          if (address.regionPrefix != undefined && address.regionPrefix != '') {
+            addressObj.region = `${address.region} ${address.regionPrefix}.`;
           }
-          if (address.areaPrefix != undefined) {
-            addressObj.area = `${address.areaPrefix}. ${address.area}`;
+          if (address.areaPrefix != undefined  && address.areaPrefix != '') {
+            addressObj.area = `${address.area} ${address.areaPrefix}.`;
           }
-          if (address.localityPrefix != undefined) {
-            addressObj.locality = `${address.localityPrefix}. ${address.locality}`;
+          if (address.localityPrefix != undefined  && address.localityPrefix != '') {
+            addressObj.locality = `${address.locality} ${address.localityPrefix}.`;
           }
-          if (address.streetPrefix != undefined) {
-            addressObj.street = `${address.streetPrefix}. ${address.street}`;
+          if (address.streetPrefix != undefined  && address.streetPrefix != '') {
+            addressObj.street = `${address.street} ${address.streetPrefix}.`;
           }
           for (var obj in addressObj) {
             if(addressObj[obj] == undefined) {
               delete addressObj[obj];
             } else {
-              personData.address.push(addressObj[obj])
+              if (!personData.address.length) {
+                personData.address.push(addressObj[obj])
+              } else {
+                personData.address.push(' ' + addressObj[obj])
+              }
             }
           }
         }
@@ -96,10 +100,16 @@ db.persons.find({
       }
       // --- Full print ---
       printString = '';
-      for(var da in personData) {
-        printString += personData[da]+';';
+      for (var it in personData) {
+        if (personData[it] == undefined) {
+          personData[it] = '';
+        }
       }
-      print(printString)
+      for(var da in personData) {
+        printString += personData[da].toString().trim() + ';';
+      }
+      printString = printString.slice(0,-1);
+      print(printString);
     } catch (err) {
       print(id, err)
     }
